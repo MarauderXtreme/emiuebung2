3789e9c4ad58a393c52df0bbe22ebb6cdba5d892
Christoph Kepler <christoph.kepler@arts.aero>
2013-11-03 15:20:38 +0100

    CSS Fixes All required form-elements Some new tasks

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -10,10 +10,41 @@ $(document).ready(
 	function () {
 		$('#email').blur(
 			function(){
+				var mailregex = 
 				var mail = $('#email').val();
-				var mailcheck = mail.match('/^[_-a-zA-Z0-9.]@[-a-zA-Z0-9.]');
-				alert(mail);
+				var mailcheck = mail.exec();
+			}
+		);
+		$('#contactsubmit').click(
+			function() {
+				var notempty;
+				var submitsuccess;
+					/**
+					 * @TODO Maybe the use of an array would be smooth
+					 */
+				if($('#name').val() == '') {
+					Array() = '';
+				}
+				if($('#email').val() == '') {
+					
+				}
+				if($('#text').val() == '') {
+					notempty = 0;
+				}
+				else {
+					notempty = 1;
+				}
+				if(notempty == 1 && mailcheck == mail) {
+					submitsuccess = 1;
+				}
+				else {
+					alert('TOD UND VERDERBEN');
+				}
 			}
 		);
 	}
 );
+/**
+ * @TODO Maybe add email encoded to js (http://stackoverflow.com/questions/246801/how-can-you-encode-a-string-to-base64-in-javascript)
+ * @TODO Add a email delivery
+ */

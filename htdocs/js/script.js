17218d31c8a3cc7be3d6b1b5409b804021e5f403
emimafia <emimafia@users.noreply.github.com>
2013-11-07 10:31:52 +0100

    Added some lines from practice

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -44,6 +44,35 @@ $(document).ready(
 		);
 	}
 );
+
+document.onmousemove = updateTooltip;
+function updateTooltip (e) {
+	if(tooltip != null && tooltip.style.display == "block") {
+		var x = (e.pageX ? e.pageX : window.event.x)
+			+ tooltip.offsetParent.scrollLeft
+			- tooltip.offsetParent.offsetLeft;
+
+		var y = (e.pageY ? e.pageY : window.event.y)
+			+ tooltip.offsetParent.scrollTop
+			- tooltip.offsetParent.offsetTop;
+
+		tooltip.style.left = (x + 20) +  "px";	
+		tooltip.style.top = (y + 20) +  "px";	
+	}
+}
+
+function stylereset() {
+	document.getElementById("style").removeAttribute("href");
+	document.getElementById("style").setAttribute("href", "css/style.css");
+}
+function validate() {
+	return true;
+}
+
+/**
+ * RegEx:
+ * var check = RegExpr;
+ * return cheack.test(mailaddress);
 /**
  * @TODO Maybe add email encoded to js (http://stackoverflow.com/questions/246801/how-can-you-encode-a-string-to-base64-in-javascript)
  * @TODO Add a email delivery
